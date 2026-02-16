ec41e7a6c0eb53604fcb9dd354755baecf47e38d
Mohammad Robiul Hossen <robim6225@gmail.com>
2026-02-16 21:22:13 +0600

    started python programming with data types and variables

---
+++ firstProgram.py
@@ -0,0 +1,49 @@
+# name = 'robi'
+# age = 25
+# gpa = 4.55
+
+# print(type(name))
+# print(type(age))
+# print(type(gpa))
+
+# if type(name) == 'str' :
+#     print('hello')
+# else:
+#     print('hi')
+
+# name = input('Enter Your Name : ')
+# age= input('Enter Your Age : ')
+
+# # message =
+# print( f"My name is {name} and I am {age} years old")
+
+# Practice 1
+# Take 2 input and print sum
+num1 = float(input('Enter Number 1: '))
+num2 = float(input('Enter Number 2: '))
+
+sum = num1 + num2
+# print(int(sum))
+
+
+# Practice 2
+# take a side of squire and calculate area 
+side = float(input('Enter a side of your squire: '))
+area = side * side
+# print(area)
+
+
+
+# Practice 3
+# Input 2 floating points and print average
+a = float(input('Enter Number 1:'))
+b = float(input('Enter Number 2:'))
+
+avg = (a+b)/2
+# print(avg)
+
+# practice 4
+# write 2 number a & b and print True if a >= b else print False 
+x = 50
+y = 100
+print(x>=y)
